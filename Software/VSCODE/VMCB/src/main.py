458c16d4b7193a49cf9ed8d34dfd0c4ebee123c5
Simon Schindler <simon.schindler@st.oth-regensburg.de>
2020-07-17 20:51:07 +0200

    Save work at rgb

--- Software/VSCODE/VMCB/src/main.py
+++ Software/VSCODE/VMCB/src/main.py
@@ -9,12 +9,13 @@ Disp.On()
 print(Disp.State())
 
 #Soll ein Bild aus dem Speicher geöffnet werden, folgenden Code benutzen
-f = open('Meier_PNG.png','rb')
+f = open('Yosemite.png','rb')
 picture_png = f.read()
 print(picture_png[0:20])
 f.close()    
 
 picture_data_spi = decompress_png.decompress_func(picture_png)
+print(len(picture_data_spi))
 Array = decompress_png.picture_info(picture_png)
 BpP=Array[2]
 
@@ -24,7 +25,7 @@ print(str(BpP), 'Picture decompressed')
 #time.sleep(1)
 import spi_waveshare97
 
-spi_waveshare97.SPI_func(picture_data_spi, BpP)
+spi_waveshare97.Simons_SPI_func(picture_data_spi, BpP)
 time.sleep(1)
 
 
